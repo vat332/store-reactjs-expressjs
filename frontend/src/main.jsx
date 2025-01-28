a3ab16c760f8d0e34978d7a7aa0ebf01f4d70a4f
vat332 <oporowymakus21@gmail.com>
2025-01-28 15:31:59 +0100

    Add Provider for redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />} />)
 );
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 );
