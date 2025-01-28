b10e108a8321c7d841a95d8aa8b3436df7cc0342
vat332 <oporowymakus21@gmail.com>
2025-01-28 23:00:55 +0100

    Add register form

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
 import {
   AiOutlineHome,
-  AiOutlineLogin,
   AiOutlineShopping,
   AiOutlineShoppingCart,
-  AiOutlineUserAdd,
 } from "react-icons/ai";
-
 import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
 import { useSelector, useDispatch } from "react-redux";
-import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { useLogoutMutation } from "../../redux/api/usersApiSlice";
 import { logout } from "../../redux/features/auth/authSlice";
 
 const Navigation = () => {
@@ -23,16 +20,19 @@ const Navigation = () => {
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
+
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   };
+
   const closeSidebar = () => {
     setShowSidebar(false);
   };
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [logoutApiCall] = useLoginMutation();
+  const [logoutApiCall] = useLogoutMutation();
+
   const logoutHandler = async () => {
     try {
       await logoutApiCall().unwrap();
@@ -42,77 +42,160 @@ const Navigation = () => {
       console.error(error);
     }
   };
-  return (
-    <div
-      style={{ zIndex: 999 }}
-      className={`${
-        showSidebar ? "hidden" : "flex"
-      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 text-white bg-black w-[4%] hover:w-[15%] h-[100vh] fixed`}
-      id="navigation-container"
-    >
-      <div className="flex items-center flex-col mt-[4rem] space-y-4">
-        <Link
-          to="/"
-          className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-        >
-          <AiOutlineHome className="mr-2" size={26} />
-          <span className="hidden sm:block">Home</span>{" "}
-        </Link>
 
-        <Link
-          to="/shop"
-          className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-        >
-          <AiOutlineShopping className="mr-2" size={26} />
-          <span className="hidden sm:block">Shop</span>{" "}
-        </Link>
-        <Link
-          to="/cart"
-          className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-        >
-          <AiOutlineShoppingCart className="mr-2" size={26} />
-          <span className="hidden sm:block">Cart</span>{" "}
-        </Link>
-        <Link
-          to="/favorite"
-          className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-        >
-          <FaHeart className="mr-2" size={26} />
-          <span className="hidden sm:block">Favorites</span>{" "}
-        </Link>
-      </div>
-      <div className="relative">
-        <button
-          onClick={toggleDropdown}
-          className="flex items-center text-gray-8000 focus:outline-none"
-        >
-          {userInfo ? (
-            <span className="text-white">{userInfo.username}</span>
-          ) : (
-            <></>
+  return (
+    <div>
+      {/* Sidebar */}
+      <div
+        style={{ zIndex: 999 }}
+        className={`${
+          showSidebar ? "flex" : "hidden"
+        } lg:flex flex-col justify-between p-4 text-white bg-black w-[4%] hover:w-[15%] h-[100vh] fixed transition-all duration-300`}
+        id="navigation-container"
+      >
+        <div className="flex flex-col mt-[4rem] space-y-4">
+          <Link
+            to="/"
+            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
+            onClick={closeSidebar}
+          >
+            <AiOutlineHome className="mr-2" size={26} />
+            <span className="ml-2">Home</span>
+          </Link>
+
+          <Link
+            to="/shop"
+            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
+            onClick={closeSidebar}
+          >
+            <AiOutlineShopping className="mr-2" size={26} />
+            <span className="ml-2">Shop</span>
+          </Link>
+          <Link
+            to="/cart"
+            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
+            onClick={closeSidebar}
+          >
+            <AiOutlineShoppingCart className="mr-2" size={26} />
+            <span className="ml-2">Cart</span>
+          </Link>
+          <Link
+            to="/favorite"
+            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
+            onClick={closeSidebar}
+          >
+            <FaHeart className="mr-2" size={26} />
+            <span className="ml-2">Favorites</span>
+          </Link>
+        </div>
+
+        {/* User Dropdown */}
+        <div className="relative">
+          <button
+            onClick={toggleDropdown}
+            className="flex items-center text-gray-800 focus:outline-none"
+          >
+            {userInfo ? (
+              <span className="text-white">{userInfo.username}</span>
+            ) : (
+              <></>
+            )}
+            {userInfo && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-4 w-4 ml-2 transform ${
+                  dropdownOpen ? "rotate-180" : ""
+                }`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+                />
+              </svg>
+            )}
+          </button>
+
+          {dropdownOpen && userInfo && (
+            <ul
+              className={`absolute right-0 mt-2 space-y-2 bg-white text-gray-600 rounded-lg shadow-lg ${
+                !userInfo.isAdmin ? "-top-20" : "-top-80"
+              }`}
+            >
+              {userInfo.isAdmin && (
+                <>
+                  <li>
+                    <Link
+                      to="/admin/dashboard"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={closeSidebar}
+                    >
+                      Dashboard
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/productlist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={closeSidebar}
+                    >
+                      Products
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/categorylist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={closeSidebar}
+                    >
+                      Category
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/orderlist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={closeSidebar}
+                    >
+                      Orders
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/admin/userlist"
+                      className="block px-4 py-2 hover:bg-gray-100"
+                      onClick={closeSidebar}
+                    >
+                      Users
+                    </Link>
+                  </li>
+                </>
+              )}
+              <li>
+                <Link
+                  to="/profile"
+                  className="block px-4 py-2 hover:bg-gray-100"
+                  onClick={closeSidebar}
+                >
+                  Profile
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={logoutHandler}
+                  className="block w-full px-4 py-2 text-left hover:bg-gray-100"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
           )}
-        </button>
+        </div>
       </div>
-      <ul className="space-y-4">
-        <li>
-          <Link
-            to="/login"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-          >
-            <AiOutlineLogin className="mr-2" size={26} />
-            <span>Login</span>{" "}
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/register"
-            className="flex items-center text-white transition-all duration-300 hover:translate-x-2"
-          >
-            <AiOutlineUserAdd className="mr-2" size={26} />
-            <span>Register</span>{" "}
-          </Link>
-        </li>
-      </ul>
     </div>
   );
 };
